ed0f37ef45818b40b5aa081ed070a64839709ea0
MateKotorashvili <matekotorashvili@gmail.com>
2024-03-02 15:49:58 +0400

    fourth commit

---
+++ day 09/homework/homework9.py
@@ -0,0 +1,10 @@
+#მომხმარებელს შემოატანინეთ ორი რიცხვი, მისი ასაკი და მშობლის ასაკი,
+#შემდეგ კი შეადარეთ ისინი ერთმანეთს
+
+
+your_age = int(input("tell us your age:   "))
+your_mom_or_dad_age = int(input("tell us your dad's or mom's age:   "))
+
+
+print(bool(your_age > your_mom_or_dad_age))
+print(bool(your_age < your_mom_or_dad_age))
