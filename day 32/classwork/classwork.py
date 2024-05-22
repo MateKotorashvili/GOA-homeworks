9d72b5e29985153f6b4e26a5807b20ce8d7e856a
MateKotorashvili <matekotorashvili@gmail.com>
2024-05-22 21:27:01 +0400

    commit

---
+++ day 32/classwork/classwork.py
@@ -0,0 +1,11 @@
+input_string = "Goa is the best"
+
+result_string = ""
+
+for i in range(len(input_string)):
+
+    if input_string[i] != " ":
+
+        result_string += input_string[i]
+
+print(result_string)
