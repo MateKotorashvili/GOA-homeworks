1d6aecbb71f5dba2b1b8396fa5157a6403e8f37f
MateKotorashvili <matekotorashvili@gmail.com>
2024-06-01 15:46:38 +0400

    commit

---
+++ day 34/homework/homework34.py
@@ -0,0 +1,24 @@
+# anbani = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+# "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+# final_letter = ["g", "o", "a"]
+
+# final_ABC = "".join([anbani for anbani in final_letter])
+
+# print(final_letter)
+
+
+# anbani = list("abcdefghijklmnopqrstuvwxyz")
+
+# letter_g = anbani[6]
+# letter_o = anbani[14]
+# letter_a = anbani[0]
+
+# final_letter = (letter_g + letter_o + letter_a)
+
+# print(final_letter)
+
+while temperature < 35: 
+    print("Hypothermia")
+while temperature > 39: 
+    print("Fever")
